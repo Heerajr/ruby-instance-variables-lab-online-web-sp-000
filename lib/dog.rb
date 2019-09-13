d17d2d00673a78b0422deb1b75c0fd36ffd352af
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-09-13 00:51:27 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name= (dog_name)
+    this_dogs_name = dog_name
+  end
+
+  def name
+    this_dogs_name
+  end
+
+  def lassie
+    lassie = Dog.new
+  end
+
+  def lassie
+    lassie.name = "lassie"
+  end
+end
